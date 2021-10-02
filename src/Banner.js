b212a04fa4f007458893a29fe220f065837c57b0
Abhishek Sahani <abhisheksahani653@gmail.com>
2021-10-02 18:01:24 +0530

    Add responsive design for banner component

--- src/Banner.js
+++ src/Banner.js
@@ -43,7 +43,7 @@ function Banner() {
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+        <h1 className="banner_description">{truncate(movie?.overview, 120)}</h1>
       </div>
       <div className="banner_fadeBottom" />
     </header>
